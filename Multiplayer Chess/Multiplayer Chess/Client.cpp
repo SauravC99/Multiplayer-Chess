f74962c25a810cb4605298681bab4c23d2be40de
TannerLow <lowthorpt@gmail.com>
2020-10-18 01:39:13 -0700

    Started working on networking

---
+++ Multiplayer Chess/Multiplayer Chess/Client.cpp
@@ -0,0 +1,22 @@
+#include "Client.h"
+
+Client::Client(const sf::IpAddress& ipAddress, unsigned short port, sf::Time timeout) {
+	if (socket.connect(ipAddress, port, timeout) != sf::Socket::Done) {
+		throw "Could not connect to server, possibly timed out";
+	}
+}
+
+void Client::send(const char* data, std::size_t bytes) {
+	if (socket.send(data, 100) != sf::Socket::Done) {
+		throw "Data could not be sent";
+	}
+}
+
+std::string Client::receive() {
+	do {
+		if (socket.receive(buffer, sizeof(buffer), received) != sf::Socket::Done)
+			throw "Error while receiving data";
+	} while (received == 0);
+
+	return std::string(buffer, received);
+}
--- Multiplayer Chess/Multiplayer Chess/main.cpp
+++ Multiplayer Chess/Multiplayer Chess/main.cpp
@@ -1,10 +1,25 @@
 #include <SFML/Graphics.hpp>
 #include "Pieces/IPiece.h"
+#include "Client.h"
+#include "Server.h"
+#include <iostream>
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1000, 750), "SFML works!");
     
+    char i;
+    std::cin >> i;
+    if (i == 'c') {
+        Client c;
+        std::cout << c.receive() << std::endl;
+        c.send("bye", 3);
+    }
+    else if (i == 's') {
+        Server s;
+        s.send("hello", 5);
+        std::cout << s.receive() << std::endl;
+    }
 
     while (window.isOpen())
     {
